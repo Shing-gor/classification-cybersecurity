8d89058c48104346bf8ecc90ad57099393e48f60
Bxluu <djbluuxx@gmail.com>
2024-10-29 12:15:21 +1100

    Naive Bayes algorithm

--- naiveBayes.py
+++ naiveBayes.py
@@ -16,21 +16,21 @@ for var in categories:
     df[var] = df[var].astype('category')
 
 X = pd.get_dummies(X, drop_first=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+XTrain, XTest, yTrain, yTest = train_test_split(X, y, test_size=0.2, random_state=10)
 
 # Fits the model on the training data using Naive Bayes
 model = GaussianNB()
-model.fit(X_train, y_train)
+model.fit(XTrain, yTrain)
 
-y_pred = model.predict(X_test)
-print(classification_report(y_test, y_pred, zero_division=0))
+yPred = model.predict(XTest)
+print(classification_report(yTest, yPred, zero_division=0))
 
 # Shows true positives, true negatives, false positives, false negatives
-cm = confusion_matrix(y_test, y_pred)
+confusionMatrix = confusion_matrix(yTest, yPred)
 
 # Heatmap visualisation of the Confusion Matrix
 plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=model.classes_, yticklabels=model.classes_)
+sns.heatmap(confusionMatrix, annot=True, fmt='d', cmap='Blues', xticklabels=model.classes_, yticklabels=model.classes_)
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
 plt.title('Confusion Matrix Heatmap')
