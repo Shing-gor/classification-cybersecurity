ca0495beaa96dfb728ffcfecd285cb50620dd720
z5419046 <z5419046@ad.unsw.edu.au>
2024-11-06 09:19:21 +1100

    Update and rename random_forest.sh to forest_v_gradient.sh

--- forest_v_gradient.sh
+++ forest_v_gradient.sh
@@ -11,7 +11,8 @@ do
 	cat cat_file4.csv | grep "$test" | shuf | sed "$test_size"q > test_file.csv
 	cat cat_file4.csv | sed "$sample_input_size"q | sed 's/,/ /g' | awk ' $2 -lt "$test" {print}' | sed 's/ /,/g' | shuf | sed "$train_size"q > train_file.csv
 	cat train_file.csv | wc -l
-	./random_forest.py train_file.csv test_file.csv | grep -v Warning > "$i".txt
+	./random_forest_ts.py train_file.csv test_file.csv | grep -v Warning > "$i"forest.txt
+	./gradient_descent_ts.py train_file.csv test_file.csv | grep -v Warning > "$i"gradient.txt
 	i=$((i + 1))
 	test_coor=$((test_corr - 1))
 	test=$(cat cat_file4.csv | cut -d',' -f2 | sort | uniq | sed -n "$test_coor"p)
